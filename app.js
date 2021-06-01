70d88fdc2ab959b69462e35b2ad3b8172fde4388
caleb <calebp80@gmail.com>
2021-05-31 17:49:58 -0700

    finished

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
+const path = require("path");
 const fs = require("fs");
-const style = require("./templates/css")
+const OUTPUT_DIR = path.resolve(__dirname,"output");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+
 
 const Employee = require("./lib/employee")
 const Engineer = require("./lib/engineer")
